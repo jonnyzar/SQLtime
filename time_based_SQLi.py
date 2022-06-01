336a60678531bc84c2a5113f672455ea735380ba
bam <bam@xak3.local>
2022-06-01 17:04:37 +0200

    initial

--- time_based_SQLi.py
+++ time_based_SQLi.py
@@ -1,3 +1,24 @@
+'''
+ Copyright (C) <2022> Yan Zaripov, https://github.com/jonnyzar/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    SECURITY DISCLAIMER:
+    This program is intended for educational or self-assessment purposes only.
+    You may have legal consequences if you use it to attack systems that do not 
+    belong to you.
+'''
+
+
 import requests
 import sys
 
